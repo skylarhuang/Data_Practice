13df09d2cb56f7962aff56d035dfd68b12c34518
Sui Huang <sh4507@nyu.edu>
2018-08-19 23:39:30 -0400

    Hashtable practice

---
+++ Hashtable/Missing_Number_I.py
@@ -0,0 +1,20 @@
+array1 = [12, 11, 10, 9, 4, 5, 6, 7, 2, 3, 8]
+
+
+def missing(array):
+    myDict = {}
+    for i in range(len(array) + 1):
+        myDict[i + 1] = 0
+
+    for number in array:
+        if number in myDict:
+            myDict[number] += 1
+
+    for k, v in myDict.items():
+        if v == 0:
+            miss = k
+    return miss
+
+
+x = missing(array1)
+print(x)
