5e5cddbacb7f20155d254074f7e1f1b0948b7766
Sui Huang <sh4507@nyu.edu>
2019-01-07 21:56:28 -0500

    Edit 2 coding files

--- 2_Array_and_Sorting_Algorithms/Selection_Sort.py
+++ 2_Array_and_Sorting_Algorithms/Selection_Sort.py
@@ -6,10 +6,26 @@ def solve(array):
     # write your solution here
     if len(array) == 1 or len(array) == 0:
         return array
-    for i in range(len(array)):
+
+    for i in range(len(array), 0, -1):
         largest_index = 0
-        for j in range(len(array) - i):
+        for j in range(i):
             if array[j] >= array[largest_index]:
                 largest_index = j
-        array[largest_index], array[len(array) - i - 1] = array[len(array) - i - 1], array[largest_index]
+        array[largest_index], array[j] = array[j], array[largest_index]
     return array
+
+
+def select_sort2(list):
+	for i in range(len(list)-1,0,-1): #-1避免0,1长度进入循环
+		max_ind = 0
+		for j in range(i+1):
+			if list[j] > list[max_ind]:
+				max_ind = j
+list[max_ind], list[i] = list[i], list[max_ind]
+
+
+x = [2,1,5,4,7,3,9]
+y = solve(x)
+
+print(y)
